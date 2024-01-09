85c63ae1dd96c6cf6be383878cd2eb8ffb88e997
Smetankin <smet-eb@yandex.ru>
2024-01-09 20:12:59 +0300

    another good day!

--- store/catalog/constants.py
+++ store/catalog/constants.py
@@ -1,7 +1,7 @@
-ITEMS_TYPES_DATA = {'trucks': [{'name': 'all types', 'icon': 'root/img/all_icon.png'},
-                               {'name': 'head', 'icon': 'root/img/icon_head.svg'},
-                               {'name': 'cargo', 'icon': 'root/img/icon_cargo.svg'},
-                               {'name': 'dump', 'icon': 'root/img/icon_dump.svg'}],
+ITEMS_TYPES_DATA = {'trucks': [{'name': 'all types', 'icon': 'img/all_icon.png'},
+                               {'name': 'head', 'icon': 'img/icon_head.svg'},
+                               {'name': 'cargo', 'icon': 'img/icon_cargo.svg'},
+                               {'name': 'dump', 'icon': 'img/icon_dump.svg'}],
                     'parts': []}
 
 HEAD = 'HEAD'
