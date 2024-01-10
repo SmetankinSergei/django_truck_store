4372c911ad1f0db0ddf79905d56e98a68e04a10a
Smetankin <smet-eb@yandex.ru>
2024-01-10 14:08:34 +0300

    another good day!

--- store/catalog/templatetags/catalog_tags.py
+++ store/catalog/templatetags/catalog_tags.py
@@ -5,6 +5,9 @@ from catalog.models import SubCatalog, Truck, Part
 register = template.Library()
 
 
+CATALOG_ITEMS = {'trucks': Truck, 'parts': Part}
+
+
 @register.simple_tag()
 def get_sub_catalogs():
     return SubCatalog.objects.all()
@@ -18,3 +21,8 @@ def get_trucks():
 @register.simple_tag()
 def get_parts():
     return Part.objects.all()
+
+
+@register.simple_tag()
+def get_catalog_items(sub_catalog_name):
+    return CATALOG_ITEMS[sub_catalog_name].objects.all()
