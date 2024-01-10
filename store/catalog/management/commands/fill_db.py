f69814289ca27bd844502c43a2198f06ce45a963
Smetankin <smet-eb@yandex.ru>
2024-01-10 22:30:41 +0300

    another good day!

--- store/catalog/management/commands/fill_db.py
+++ store/catalog/management/commands/fill_db.py
@@ -5,30 +5,24 @@ from django.core.management import BaseCommand
 
 from catalog.models import Part, TruckInfo, Truck, SubCatalog
 
-TABLES_LIST = [Part, TruckInfo, Truck, SubCatalog]
+TABLES_DICT = {'subcatalog': SubCatalog, 'part': Part, 'truckinfo': TruckInfo, 'truck': Truck}
 TABLES_DATAFILES = ['sub_catalog_data.json', 'part_data.json', 'truck_info_data.json', 'truck_data.json']
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        # for table in TABLES_LIST:
-        #     table.objects.all().delete()
+        for table in TABLES_DICT.values():
+            table.objects.all().delete()
 
-        # for datafile in TABLES_DATAFILES:
-        #     path = os.path.join('datafiles', datafile)
-        #
-        #     with open(path) as file:
-        #         tables_data = json.load(file)
-        #
-        #         print(type(tables_data))
-        #         print(tables_data)
+        for datafile in TABLES_DATAFILES:
 
-        path = os.path.join('datafiles', 'truck_data.json')
+            path = os.path.join('datafiles', datafile)
 
-        with open(path) as file:
-            tables_data = json.load(file)
-            for item in tables_data:
-                del item['model']
-                print(item)
-            # print(type(tables_data))
-            # print(tables_data)
+            with open(path) as file:
+                tables_data = json.load(file)
+                preloading_list = []
+                table = None
+                for item in tables_data:
+                    table = TABLES_DICT[item['model'].split('.')[1]]
+                    preloading_list.append(table(**item['fields']))
+                table.objects.bulk_create(preloading_list)
