2cae03c79e6c8f1ae203d8d8ba3bc068b67bfc85
Smetankin <smet-eb@yandex.ru>
2024-01-10 23:08:27 +0300

    another good day!

--- store/catalog/templatetags/catalog_tags.py
+++ store/catalog/templatetags/catalog_tags.py
@@ -1,6 +1,7 @@
 from django import template
 
 from catalog.models import SubCatalog, Truck, Part
+from store.settings import MEDIA_URL
 
 register = template.Library()
 
@@ -26,3 +27,13 @@ def get_parts():
 @register.simple_tag()
 def get_catalog_items(sub_catalog_name):
     return CATALOG_ITEMS[sub_catalog_name].objects.all()
+
+
+@register.simple_tag()
+def main_photo_path_tag(item):
+    return item.main_photo.url
+
+
+@register.filter()
+def main_photo_path(item_photo_path):
+    return item_photo_path.url
