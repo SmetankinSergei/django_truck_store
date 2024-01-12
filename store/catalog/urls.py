a78577fbe69f18be466ac08262866aee81aadadb
Smetankin <smet-eb@yandex.ru>
2024-01-12 18:08:49 +0300

    another good day!

--- store/catalog/urls.py
+++ store/catalog/urls.py
@@ -6,7 +6,7 @@ app_name = 'catalog'
 
 urlpatterns = [
     path('', views.catalog, name='catalog'),
-    path('sub_catalog/<str:sub_catalog_name>/<str:current_type>/<str:prev_item_type>/',
+    path('sub_catalog/<str:sub_catalog_name>/<str:current_item_type>/<str:prev_item_type>/',
          views.sub_catalog, name='sub_catalog'),
     path('item/<int:item_id>/<str:current_catalog>/', views.item, name='item'),
 ]
--- store/catalog/views.py
+++ store/catalog/views.py
@@ -9,30 +9,30 @@ def catalog(request):
     return render(request, 'catalog/catalog.html')
 
 
-def sub_catalog(request, sub_catalog_name, current_type, prev_item_type):
-    page_data = {
+def sub_catalog(request, sub_catalog_name, current_item_type, prev_item_type):
+    context = {
         'title': 'MAN - Catalog',
         'section_name': 'catalog',
         'previous_section': 'catalog',
-        'sub_catalog_name': sub_catalog_name,
         'is_sub_section': True,
+        'sub_catalog_name': sub_catalog_name,
         'types_data': ITEMS_TYPES_DATA[sub_catalog_name],
-        'current_type': current_type,
+        'current_item_type': current_item_type,
         'prev_item_type': prev_item_type,
-        'items': get_catalog_items(sub_catalog_name, current_type),
+        'items': get_catalog_items(sub_catalog_name, current_item_type),
     }
-    return render(request, 'catalog/sub_catalog.html', page_data)
+    return render(request, 'catalog/sub_catalog.html', context)
 
 
 def item(request, item_id, current_catalog):
     CATALOG_TABLES = {'trucks': Truck, 'parts': Part}
     catalog_item = CATALOG_TABLES[current_catalog].objects.get(pk=item_id)
-    page_data = {
+    context = {
         'title': 'MAN - Catalog item',
         'section_name': 'catalog',
         'previous_section': 'catalog',
-        'current_catalog': current_catalog,
         'is_sub_section': True,
+        'current_catalog': current_catalog,
         'item': catalog_item,
     }
-    return render(request, 'catalog/item.html', page_data)
+    return render(request, 'catalog/item.html', context)
