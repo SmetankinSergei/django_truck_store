2e8ddc7790816c8cfae73cf49b38c4caa9f67e81
Smetankin <smet-eb@yandex.ru>
2024-01-12 19:50:34 +0300

    another good day!

--- store/catalog/views.py
+++ store/catalog/views.py
@@ -22,8 +22,9 @@ def sub_catalog(request, sub_catalog_name, current_item_type, prev_item_type):
 
 def item(request, item_id, sub_catalog_name):
     CATALOG_TABLES = {'trucks': Truck, 'parts': Part}
+    SUB_CATALOG_TEMPLATES = {'trucks': 'catalog/truck_item.html', 'parts': 'catalog/part_item.html',}
     catalog_item = CATALOG_TABLES[sub_catalog_name].objects.get(pk=item_id)
     context = {
         'item': catalog_item,
     }
-    return render(request, 'catalog/item.html', {**context, **SUB_CATALOG_CONTEXT})
+    return render(request, SUB_CATALOG_TEMPLATES[sub_catalog_name], {**context, **SUB_CATALOG_CONTEXT})
